43fa4ab746b7f61828c5c626db3fbfb0e256846d
Alimilan88 <162614275+Alimilan88@users.noreply.github.com>
2024-06-07 10:20:27 +0000

    premera

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  console.log('hello');
+  newCard()
 };
+
+function newCard() {
+  let suits = ['♦', '♥', '♠', '♣'];
+  let numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+  let indexSuits = Math.floor(Math.random() * suits.length);
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+
+  document.querySelector('#top').innerHTML = suits[indexSuits]
+  document.querySelector('#number').innerHTML = numbers[indexNumbers]
+  document.querySelector('#end').innerHTML = suits[indexSuits]
+
+  // Cuando suits sea '♦' o '♥' cambiar la clase de top y end a text-danger
+  let colorSuit = false ? "text-danger" : "text-dark"
+  document.querySelector('#top').className = colorSuit
+  document.querySelector('#number').className = colorSuit
+  document.querySelector('#end').className = colorSuit
+
+  console.log('new card', indexSuits, indexNumbers)
+}
